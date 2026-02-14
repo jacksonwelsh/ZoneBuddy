142d4af7e9149d68a77059c92524b668385f072d
Jackson Welsh <me@jacksonwel.sh>
2026-02-13 21:03:53 -0800

    initial commit

---
+++ ZoneBuddyTests/ZoneBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZoneBuddyTests.swift
+//  ZoneBuddyTests
+//
+//  Created by Jackson Welsh on 2/13/26.
+//
+
+import Testing
+@testable import ZoneBuddy
+
+struct ZoneBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
